14084a8e50b34c328ce069671a312aaf8f8a3f52
Geronimo Orozco <gorozco@gmail.com>
2008-02-13 03:19:08 +0000

    Added a blank line
    git-svn-id: http://svn.code.sf.net/p/mosat/code/trunk@27 becb1130-6c36-0410-badf-e7708b0ee230

--- mosat/include/rtcommon.h
+++ mosat/include/rtcommon.h
@@ -1,4 +1,5 @@
 /***************************************************************************
+
 Copyright (c) 2002,2003,2004 Donald Wayne Carr 
 
 Permission is hereby granted, free of charge, to any person obtaining a 
