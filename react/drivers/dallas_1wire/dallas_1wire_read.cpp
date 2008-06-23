d3c774fd5056833a7d8bd44206be0a6a1aa694ac
Donald Carr <doncarr@gmail.com>
2008-06-23 18:01:30 +0000

    Added the skeleton for a multi-threaded driver for Dallas 1-Wire
    git-svn-id: http://svn.code.sf.net/p/mosat/code/trunk@192 becb1130-6c36-0410-badf-e7708b0ee230

---
+++ react/drivers/dallas_1wire/dallas_1wire_read.cpp
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+
+#include "dallas_1wire.h"
+
+/* This function will return the temperature in degrees celcius as 
+ * double floating point.
+ */
+
+int init_dallas_wire_temperatures(char *error, int size)
+{
+}
+
+int read_dallas_wire_temperatures(double *temperatures, int max, 
+           char *error, int size)
+{
+}
+
