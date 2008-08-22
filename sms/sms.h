cd5ccd51de24e91eb166b11544f8bacfaf09d902
thaumiel999 <thaumiel999@becb1130-6c36-0410-badf-e7708b0ee230>
2008-08-22 19:47:45 +0000

    added a little explanation of the two principal classes in sms.h
    git-svn-id: http://svn.code.sf.net/p/mosat/code/trunk@231 becb1130-6c36-0410-badf-e7708b0ee230

--- sms/sms.h
+++ sms/sms.h
@@ -1,3 +1,72 @@
+/*
+Relevant function calls
+
+sms::sms(const char *host, const char *user, const char *password, const char *database, const char *smsdConfig, const char *receiveFile, const char *sendFile);
+
+Its the constructor, initialize the object, creates the connection with mysql, and reads the files receive and send
+
+
+bool sms::sms_send(const char *message, const char *number);
+
+	Sends a message to an arbitrary number, returns true if success
+
+
+bool sms::sms_send_member(const char *message, const char *name, const char *group);
+
+	Sends a message to a member of the given group, the group its defined in the file send.conf, returns true if success
+	
+bool sms::sms_send_group(const char *message, const char *group);
+	
+	Sends  a message to the entire given group if present in the send.conf file, return true if success
+	
+bool sms::sms_send_all(const char *message);
+
+	Sends a message to all the numbers in the send.conf file, returns true if success
+	
+smsMessage sms::next_sms();
+
+	Retrieve the next message of any number, present or not in the receive.conf file, returns a smsMessage object
+	
+smsMessage sms::next_sms_number(const char *number);
+	
+	Retrieve the next message from the given number, present or not in the receive.conf file, return a smsMessage object
+
+smsMessage sms::next_sms_member(const char *name, const char *group);
+
+	Retrive the next message from the given member of the given group defined in the receive.conf file, returns a smsMessage object
+	
+smsMessage sms::next_sms_group(const char *group);
+
+	Retrieve the next message from any member of the given group defined in the receive.conf file, returns a smsMessage object
+	
+void sms::sms_prueba();
+
+	Metod with a never ending loop that keeps checking for new message every 10 seconds and responds, to the number with the message received, it will be modified soit responds with information about the prototype if receives a well formed message, and the number its a member of the group.
+
+const char *smsMessage::getMessage();
+	
+	Retrives the message, returns a constant string
+	
+const char *smsMessage::getNumber();
+
+	Retrieves the number of the sender, returns a constant string
+
+const char *smsMessage::getName();
+
+	Retrieves the name of the sender, the name will be a "@" if no name present, returns a constant string
+	
+const char *smsMessage::getGroup();
+
+	Retrives the groups of the sender, the group will be a "@" if no group present, returns a constant string
+	
+bool smsMessage::success();
+	
+	Returns true if there is a message, false if there is no message
+	
+*/
+
+
+
 #include <mysql.h>
 #include <stdio.h>
 #include "rtcommon.h"
