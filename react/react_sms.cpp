b1b8dd7170576d9d9fd1282e8f8b194c366d9031
Donald Carr <doncarr@gmail.com>
2008-08-23 16:24:01 +0000

    Forgot to add the file
    git-svn-id: http://svn.code.sf.net/p/mosat/code/trunk@233 becb1130-6c36-0410-badf-e7708b0ee230

---
+++ react/react_sms.cpp
@@ -0,0 +1,40 @@
+/************************************************************************
+ * This software is part of React, a control engine
+ * Copyright (C) 2008 Donald Wayne Carr, Carlos René Diaz 
+ *
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * ***********************************************************************/
+
+#include <stdio.h>
+
+#include "db_point.h"
+#include "db.h"
+
+
+void db_point_t::send_sms_group(const char *msg, const char *group)
+{
+  printf("Message for %s: %s\n", group, msg);
+}
+
+void react_t::init_sms(void)
+{
+
+}
+
+void react_t::check_sms(void)
+{
+
+}
+
