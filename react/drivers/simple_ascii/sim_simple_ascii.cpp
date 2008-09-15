76a76f386b7e66f43b1f8d1d21cd98be6ee89d96
Donald Carr <doncarr@gmail.com>
2008-09-15 14:29:09 +0000

    added simple ascii driver
    git-svn-id: http://svn.code.sf.net/p/mosat/code/trunk@261 becb1130-6c36-0410-badf-e7708b0ee230

---
+++ react/drivers/simple_ascii/sim_simple_ascii.cpp
@@ -0,0 +1,74 @@
+
+
+#include <stdio.h>
+
+#include "rt_serial.h"
+
+#include "simple_ascii.h"
+
+int main(int argc, char *argv[])
+{
+   int device;
+
+   char dev_name[60];
+   if (argc > 1)
+   {
+     snprintf(dev_name, sizeof(dev_name), "/dev/%s", argv[1]);
+   }
+   else
+   {
+     snprintf(dev_name, sizeof(dev_name), "/dev/ttyUSB0");
+   }
+
+   rt_verbose = 1;
+   printf("Opening device %s . . . \n", dev_name);
+   device = rt_open_serial(dev_name,9600,0.0);
+   printf("Device opened: %d \n", device);
+
+
+   char data[200];
+   int ret_val;
+
+  while(true)
+  {
+    ret_val = rt_read_serial(device, data, 1); 
+    if (ret_val < 0)
+    {
+      printf("Error reading serial port\n");
+    }
+    else
+    {
+      printf("Got this: %s\n", data);
+    }
+
+    if (data[0] == 'R')
+    {
+      write(device, "04\n", 3);
+      write(device, "+12.345678\n", 11);
+      write(device, "+12.345678\n", 11);
+      write(device, "+12.345678\n", 11);
+      write(device, "+12.345678\n", 11);
+      write(device, "101010101001\n", 13);
+      write(device, "010000000100\n", 13);
+    }
+    else if (data[0] == 'W')
+    {
+      ret_val = rt_read_serial(device, data, 3); 
+      if (ret_val < 0)
+      {
+        printf("Error reading serial port\n");
+        write(device, "01", 2);
+      }
+      int ch = data[1] - '0'; 
+      ch += (data[0] - '0') * 10;
+      bool val = (data[2] == '1');
+      printf("%s channel %d\n", val ? "Turning On" : "Turning OFF", ch);
+      write(device, "OK", 2);
+    }
+    else
+    {
+      printf("You FOOL, that is NOT correct\n");
+      write(device, "02", 2);
+    }
+  }
+}
---
+++ react/drivers/simple_ascii/simple_ascii_read.cpp
@@ -0,0 +1,102 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "rt_serial.h"
+#include "simple_ascii.h"
+
+
+int init_simple_ascii(const char *device, char *error, int size)
+{
+  int serial_fd; 
+  rt_verbose = 1;
+  printf("Opening device %s . . . \n", device);
+  serial_fd = rt_open_serial(device ,9600,0.0);
+  printf("Serial fd: %d \n", serial_fd);
+  if (serial_fd < 0)
+  {
+    perror(device);
+  }
+  return serial_fd;
+}
+
+/******************************************************************/
+
+int read_simple_ascii(int fd, 
+           double *ai_vals, int max_ai, 
+           bool *di_vals, int max_di, 
+           bool *do_vals, int max_do, 
+           char *error, int size)
+{
+  char buf[100];
+  write(fd, "R", 1);
+  int ret_val = rt_read_serial(fd, buf, 3);
+  if (ret_val < 0)
+  {
+    printf("Error reading serial port\n");
+  }
+  int n = atol(buf);
+  for (int i=0; ((i < n) && (i < max_ai)); i++)
+  {
+    ret_val = rt_read_serial(fd, buf, 11);
+    if (ret_val < 0)
+    {
+      printf("Error reading serial port\n");
+    }
+    ai_vals[i] = atof(buf);
+  }
+
+  ret_val = rt_read_serial(fd, buf, 13);
+  if (ret_val < 0)
+  {
+    printf("Error reading serial port\n");
+  }
+  for (int i=0; i < 12; i++)
+  {
+    di_vals[i] = (buf[i] == '1');
+  }
+  
+  ret_val = rt_read_serial(fd, buf, 13);
+  if (ret_val < 0)
+  {
+    printf("Error reading serial port\n");
+  }
+  for (int i=0; i < 12; i++)
+  {
+    do_vals[i] = (buf[i] == '1');
+  }
+  
+  return 0;
+  return 0;
+}
+
+/******************************************************************/
+
+int send_simple_ascii(int fd, int channel, bool val,        
+           char *error, int size)
+{
+  char buf[10];
+  write(fd, "R", 1);
+  int ret_val = rt_read_serial(fd, buf, 3);
+  if (ret_val < 0)
+  {
+    printf("Error reading serial port\n");
+  }
+  if (0 == strncasecmp(buf, "OK", 2))
+  {
+    return 0;
+  }
+  else
+  {
+    int err = atoi(buf);
+    printf("******* Error sending do: %d\n", err);
+    return err;
+  }
+  return 0;
+}
+
