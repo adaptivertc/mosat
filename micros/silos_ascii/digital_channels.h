3a38d14601bc6db89a8a4667f5960495132ac0ea
thaumiel999 <thaumiel999@becb1130-6c36-0410-badf-e7708b0ee230>
2009-12-07 03:46:50 +0000

    First version of the silo sequences with the ascii protocol, hopefully it will work.
    git-svn-id: http://svn.code.sf.net/p/mosat/code/trunk@383 becb1130-6c36-0410-badf-e7708b0ee230

---
+++ micros/silos_ascii/digital_channels.h
@@ -0,0 +1,22 @@
+/*Outputs*/
+#define FAN_ONE_ON 0
+#define FAN_ONE_OFF 1
+#define FAN_TWO_ON 2
+#define FAN_TWO_OFF 3
+#define FAN_THREE_ON 4
+#define FAN_THREE_OFF 5
+#define FAN_FOUR_ON 6
+#define FAN_FOUR_OFF 7
+#define FAN_FIVE_ON 8
+#define FAN_FIVE_OFF 9
+#define FAN_SIX_ON 10
+#define FAN_SIX_OFF 11
+
+
+/*Inputs*/
+#define SWITCH_AUTO 0
+#define SWITCH_ON 1
+
+/*Virtual outputs*/
+
+#define CANCEL_AUT 50
