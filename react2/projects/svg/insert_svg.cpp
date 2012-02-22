ac0c496840db2d616f9235d1b05af31ea25705a7
Donald Carr <doncarr@gmail.com>
2012-02-22 20:51:00 +0000

    Added object types only_text and insert_svg
    git-svn-id: http://svn.code.sf.net/p/mosat/code/trunk@457 becb1130-6c36-0410-badf-e7708b0ee230

---
+++ react2/projects/svg/insert_svg.cpp
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "gen_display.h"
+
+static int n_instance = 1;
+
+class insert_svg_t : public gen_object_base_t
+{
+public:
+  const char *get_name(void); 
+  void generate(FILE *svg_fp, FILE *js_fp, int argc, char **argv);
+};
+
+extern "C" gen_object_base_t *get_object(void)
+{
+  return new insert_svg_t;
+}
+
+const char *insert_svg_t::get_name(void)
+{
+  return "insert_svg";
+}
+
+void insert_svg_t::generate(FILE *svg_fp, FILE *js_fp, int argc, char **argv)
+{
+  const char *the_svg = argv[1];
+  
+  fprintf(svg_fp, "<!--  START insert for insert_svg (%03d) -->\n", n_instance);
+  fprintf(svg_fp, "%s\n", the_svg); 
+  fprintf(svg_fp, "<!--  END insert for insert_svg (%03d) -->\n", n_instance);
+  n_instance++;
+}
+
